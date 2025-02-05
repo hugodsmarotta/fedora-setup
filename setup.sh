ff74b95e02a1ea68fb401d5aeb15074307f64b2b
Hugo Marotta <hugodsmarotta@proton.me>
2025-02-05 20:52:58 -0300

    Limit DNF to 10 parallel downloads

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ set -eo pipefail
 
 # Disable sudo timeout
 echo "Defaults timestamp_timeout = -1" | sudo tee /etc/sudoers.d/timeout >/dev/null
+
+# Limit DNF to 10 parallel downloads
+echo "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf >/dev/null
