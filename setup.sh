835c97011fb8924e60a9ee1658cb08683fe26113
Hugo Marotta <hugodsmarotta@proton.me>
2025-02-05 22:05:48 -0300

    Enable Flathub repository

--- setup.sh
+++ setup.sh
@@ -12,3 +12,6 @@ echo "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf >/dev/null
 sudo dnf install -y \
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
+
+# Enable Flathub repository
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
