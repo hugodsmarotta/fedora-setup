5133ad0bb5c0ba4bd3ff3ad740e8846163bc63e2
Hugo Marotta <hugodsmarotta@proton.me>
2025-02-05 21:00:24 -0300

    Enable RPM Fusion repositories

--- setup.sh
+++ setup.sh
@@ -7,3 +7,8 @@ echo "Defaults timestamp_timeout = -1" | sudo tee /etc/sudoers.d/timeout >/dev/n
 
 # Limit DNF to 10 parallel downloads
 echo "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf >/dev/null
+
+# Enable RPM Fusion repositories
+sudo dnf install -y \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
