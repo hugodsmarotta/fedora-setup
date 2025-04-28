9b2f5affd1b9e14e5afbc8f8e8e482444115ab79
Hugo Marotta <hugodsmarotta@proton.me>
2025-04-28 13:51:41 -0300

    Check if the OS is Fedora Workstation 41 or higher

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 set -eo pipefail
+
+. /etc/os-release
+
+if [[ "$ID" != 'fedora' || "$VARIANT_ID" != 'workstation' || "$VERSION_ID" < 41 ]]; then
+  echo 'This script is intended for Fedora Workstation 41 or higher.'
+  exit 1
+fi
