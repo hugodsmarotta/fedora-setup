1cd2ef9a8afa960e8ff7cda821a0010dd4301b9b
Hugo Marotta <hugodsmarotta@proton.me>
2025-04-28 14:00:41 -0300

    Remove unnecessary packages

--- setup.sh
+++ setup.sh
@@ -10,3 +10,33 @@ if [[ "$ID" != 'fedora' || "$VARIANT_ID" != 'workstation' || "$VERSION_ID" < 41 
 fi
 
 echo 'Defaults timestamp_timeout = -1' | sudo tee /etc/sudoers.d/timeout >/dev/null
+
+sudo dnf remove -y \
+  baobab \
+  firefox* \
+  gnome-abrt \
+  gnome-boxes \
+  gnome-calendar \
+  gnome-characters \
+  gnome-clocks \
+  gnome-color-manager \
+  gnome-connections \
+  gnome-contacts \
+  gnome-disk-utility \
+  gnome-font-viewer \
+  gnome-logs \
+  gnome-maps \
+  gnome-system-monitor \
+  gnome-tour \
+  gnome-weather \
+  ibus-anthy \
+  ibus-hangul \
+  ibus-libpinyin \
+  ibus-m17n \
+  ibus-typing-booster \
+  libreoffice* \
+  mediawriter \
+  rhythmbox \
+  simple-scan \
+  snapshot \
+  yelp
